06ee2fb71b5efafdfdbdcf32379fb5895acf97fb
Filip Nekic <filip.nekic@fer.hr>
2021-04-13 00:52:05 +0200

    Login dovrsen

---
+++ QuizApp/QuizzesViewController.swift
@@ -0,0 +1,137 @@
+//
+//  QuizzesViewController.swift
+//  QuizApp
+//
+//  Created by five on 12/04/2021.
+//
+
+import Foundation
+import UIKit
+
+class QuizzesViewController: UIViewController {
+    
+    private var headerText: UILabel!
+    private var getQuizzesButton: UIButton!
+    private var quizzes: [Quiz]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildViews()
+        addConstraints()
+    }
+
+    private func buildViews() {
+        view.backgroundColor = UIColor(red:0.49, green: 0.26, blue: 0.96, alpha: 1.0)
+
+        headerText = UILabel()
+        headerText.text = "PopQuiz"
+        headerText.textColor = .white
+        headerText.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.bold)
+        headerText.textAlignment = .center
+
+        
+//        let defaultTextAttributes:[NSAttributedString.Key: Any] = [
+//            .foregroundColor: UIColor.white,
+//            .font: UIFont.systemFont(ofSize: 17.0, weight: UIFont.Weight.bold)
+//        ]
+//        let placeHolderAttr:[NSAttributedString.Key :Any] = [
+//            .foregroundColor: UIColor.gray,
+//            .font: UIFont.systemFont(ofSize: 17.0, weight: UIFont.Weight.regular)
+//        ]
+        
+//        usernameTextInput = UITextField()
+//        let paddingForUsernameText = UIView(frame:CGRect(x:0, y:0, width: 20, height: usernameTextInput.frame.height))
+//        usernameTextInput.defaultTextAttributes = defaultTextAttributes
+//        usernameTextInput.attributedPlaceholder = NSAttributedString(string: "Email", attributes: placeHolderAttr)
+//        usernameTextInput.backgroundColor = UIColor(red:1.0, green: 1.0, blue: 1.0, alpha: 0.6)
+//        usernameTextInput.layer.cornerRadius = 26.0
+//        usernameTextInput.leftView = paddingForUsernameText
+//        usernameTextInput.leftViewMode = .always
+//        usernameTextInput.rightView = paddingForUsernameText
+//        usernameTextInput.rightViewMode = .always
+//        usernameTextInput.addTarget(self, action: #selector(textFieldBorder), for: .editingDidBegin)
+//        usernameTextInput.addTarget(self, action: #selector(textFieldBorder), for: .editingDidEnd)
+//        usernameTextInput.addTarget(self, action: #selector(loginButtonEnabler), for: .editingChanged)
+//        usernameTextInput.autocapitalizationType = .none
+//
+       
+
+//        passwordTextInput = UITextField()
+//        let paddingForPasswordText = UIView(frame:CGRect(x:0, y:0, width: 20, height: passwordTextInput.frame.height))
+//        passwordTextInput.defaultTextAttributes = defaultTextAttributes
+//        passwordTextInput.attributedPlaceholder = NSAttributedString(string: "Password", attributes: placeHolderAttr)
+//        passwordTextInput.backgroundColor = UIColor(red:1.0, green: 1.0, blue: 1.0, alpha: 0.6)
+//        passwordTextInput.layer.cornerRadius = 26.0
+//        passwordTextInput.isSecureTextEntry = true
+//        passwordTextInput.leftView = paddingForPasswordText
+//        passwordTextInput.leftViewMode = .always
+//        passwordTextInput.rightView = paddingForPasswordText
+//        passwordTextInput.rightViewMode = .always
+//        passwordTextInput.addTarget(self, action: #selector(textFieldBorder), for: .editingDidBegin)
+//        passwordTextInput.addTarget(self, action: #selector(textFieldBorder), for: .editingDidEnd)
+//        passwordTextInput.addTarget(self, action: #selector(loginButtonEnabler), for: .editingChanged)
+        
+        
+        getQuizzesButton = UIButton()
+        let loginAttributes :[NSAttributedString.Key:Any] = [
+            .font : UIFont.systemFont(ofSize: 17.0, weight:UIFont.Weight.bold),
+            .foregroundColor: UIColor.purple
+        ]
+        getQuizzesButton.setAttributedTitle(NSAttributedString(string: "Get Quiz", attributes: loginAttributes), for: .normal)
+        getQuizzesButton.layer.cornerRadius = 26.0
+        getQuizzesButton.addTarget(self, action: #selector(getQuizzes), for: .touchUpInside)
+        getQuizzesButton.backgroundColor = .white
+        
+        view.addSubview(headerText)
+//        view.addSubview(usernameTextInput)
+//        view.addSubview(passwordTextInput)
+        view.addSubview(getQuizzesButton)
+    }
+
+    private func addConstraints() {
+        headerText.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            headerText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            headerText.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            headerText.heightAnchor.constraint(equalToConstant: 70),
+            headerText.widthAnchor.constraint(equalTo: view.widthAnchor,constant: -20 )
+        ])
+
+//        usernameTextInput.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            usernameTextInput.topAnchor.constraint(equalTo: headerText.bottomAnchor, constant: 100),
+//            usernameTextInput.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+//            usernameTextInput.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -60),
+//            usernameTextInput.heightAnchor.constraint(equalToConstant: 50)
+//
+//        ])
+
+//        passwordTextInput.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            passwordTextInput.topAnchor.constraint(equalTo: usernameTextInput.bottomAnchor, constant: 20),
+//            passwordTextInput.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+//            passwordTextInput.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -60),
+//            passwordTextInput.heightAnchor.constraint(equalToConstant: 50)
+//
+//        ])
+        
+        getQuizzesButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            getQuizzesButton.topAnchor.constraint(equalTo: headerText.bottomAnchor, constant: 10),
+            getQuizzesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            getQuizzesButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -60),
+            getQuizzesButton.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+    }
+    
+    @objc func getQuizzes() {
+        let dataService = DataService()
+        quizzes = dataService.fetchQuizes()
+        print(quizzes!)
+    }
+    
+    
+
+   
+}
+
